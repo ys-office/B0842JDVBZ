066e94032d8d6856e9fcbc1b1a6d1e92225dba04
Yusuke Sato <yusuke.sato@ys-office.me>
2021-01-02 10:58:43 +0900

    add a self writing code

---
+++ zero-scratch/hello.py
@@ -0,0 +1,6 @@
+# 関数を定義
+def func(message):
+    print(message)
+
+# 定義した関数の呼び出し
+func('こんにちは')
---
+++ zero-scratch/test_hello.py
@@ -0,0 +1,7 @@
+# unittest モジュールを読み込んで利用
+import unittest
+
+class TestFunc(unittest.TestCase):
+    def test_func(self):
+        from hello import func
+        self.assertIsNone(func('こんにちは'))
